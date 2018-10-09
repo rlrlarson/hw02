de375e909e21521097398402551e0660af5b2098
rlrlarson <rebecca.larson@tufts.edu>
2018-10-08 20:55:44 -0400

    updated routes.py to be for heroku and recreated virtual env

--- routes.py
+++ routes.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from models import db, User
 from forms import UsersForm
+from flask_heroku import Heroku
 
 app = Flask(__name__)
+heroku = Heroku(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rebeccalarson:test_pw@localhost:5432/homework_users'
+#removing to deploy to heroku
+#app = Flask(__name__)
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rebeccalarson:test_pw@localhost:5432/homework_users'
 
 db.init_app(app)
 
